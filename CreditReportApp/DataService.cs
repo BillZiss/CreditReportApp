3d781114adf71d42f03b7793c06f7ec98b4b64ab
Bill Ziss <bill@billziss.com>
2024-09-29 12:00:56 -0400

    initial full commit

---
+++ CreditReportApp/DataService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using CreditReportApp.Components;
+using CreditReportApp.Models;
+using Microsoft.AspNetCore.Components.Forms;
+using static System.Net.WebRequestMethods;
+using Microsoft.AspNetCore.Components;
+
+namespace CreditReportApp
+{
+    public static class DataService
+    {
+        public async static Task<List<CreditRow>> GetCreditRowsAsync()
+        {
+            var http = new HttpClient();
+            string json = await http.GetStringAsync("http://localhost:5144/CreditRows.json");
+            List<CreditRow> creditRows = JsonConvert.DeserializeObject<List<CreditRow>>(json);
+            return creditRows;
+        }
+
+        public static CreditDetail GetCreditDetails()
+        {
+            CreditDetail creditDetail = new CreditDetail
+            {
+                Id = 23892389,
+                Age = 37,
+                Occupation = "welder",
+                State = "OH"
+            };
+
+            return creditDetail;
+
+        }
+    }
+}
---
+++ CreditReportApp/Models/CreditRow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditReportApp.Models
+{
+    public class CreditRow
+    {
+        public CreditRow(int id, string firstName, string lastName, string creditAgency, int creditScore, int age, string state, string occupation)
+        {
+            Id  = id;
+            FirstName = firstName;
+            LastName = lastName;
+            CreditAgency = creditAgency;
+            CreditScore = creditScore;
+            Age = age;
+            State = state;
+            Occupation = occupation;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CreditAgency { get; set; }
+        public int CreditScore { get; set; }
+        public int Age { get; set; }
+        public string State { get; set; }
+        public string Occupation { get; set; }
+    }
+}
